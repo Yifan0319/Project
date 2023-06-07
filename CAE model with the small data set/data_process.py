f6e37d6f7d1df6e11db8efc09ca80a104b2a6806
Yifan0319 <117282701+Yifan0319@users.noreply.github.com>
2023-06-07 14:09:57 +0100

    Add files via upload

--- CAE model with the small data set/data_process.py
+++ CAE model with the small data set/data_process.py
@@ -4,9 +4,9 @@ import numpy as np
 from scipy.fftpack import fft,ifft
 from scipy import signal
 
-#Initialization
-file_path = r"data\ev0000364000.h5"
-
+# Initialization
+# file_path = r"data\ev0000364000.h5"
+file_path = r"/Users/wuyifan/Desktop/project/ev0000364000.h5"
 # Open the file
 f = h5py.File(file_path, 'r')
 
@@ -27,17 +27,20 @@ end = 25*3600*100
 # Number of channels
 num_channels = 3
 
-#extract every 5min data in each hour
+# extract every 5min data in each hour
 data = np.empty((cnt,num_channels,calc_num,))
 for i in range(cnt):
     data[i,0:num_channels,:] = f[stations_list[0]][0:num_channels,0+hour_interval*i:calc_num+hour_interval*i]
 
-#convert the extracted data with stft method   
+# convert the extracted data with stft method
 data_processed = np.empty((251,16,cnt,))
 for i in range(cnt):
     freq, t, nd = signal.stft(data[i,1,:] ,fs = 200,window ='hann',nperseg = 500,noverlap = 300)
     data_processed[:,:,i] = nd
 
-#save the processed data	
-np.save('data\data.npy', data)
-np.save('data\data_processed.npy', data_processed)
+# save the processed data
+# np.save('data\data.npy', data)
+# np.save('data\data_processed.npy', data_processed)
+np.save('/Users/wuyifan/PycharmProjects/CAE model/data.npy', data)
+np.save('/Users/wuyifan/PycharmProjects/CAE model/data_processed.npy', data_processed)
+
